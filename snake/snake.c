153c9133774bf6973b9f0585aeec27930f513ee5
Fan Yu <3449435020@qq.com>
2026-04-28 10:29:09 +0800

    deleted:    snake.c 	deleted:    snake.h 	new file:   snake/snake.c 	new file:   snake/snake.h 	new file:   snake/snake_main.c 	deleted:    snake_main.c

---
+++ snake/snake.c
@@ -0,0 +1,39 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+#define MAP_HEIGHT 20
+#define MAP_WIDTH 40
+#define UP 'w'
+#define DOWN 's'
+#define LEFT 'a'
+#define RIGHT 'd'
+
+typedef struct {
+    int x;
+    int y;
+} Food, Snakenode;
+
+typedef struct {
+    Snakenode body[1000];
+    int length;
+    int speed;
+} Snake;
+
+void GotoXY(int x, int y);  // Move the cursor to the specified coordinates
+void Hide();                // Hide the cursor
+int Menu();                 // Display the menu and return the user's choice
+void Help();                // Display help information
+void About();               // Display about information
+void InitMap();             // Initialize the game map
+void PrintFood();           // Print the food on the map
+int MoveSnake();            // Move the snake based on user input
+int IsCorrect();            // Check if the snake's move is valid
+void SpeedControl();        // Control the speed of the snake
+
+int main(){
+
+    return 0;
+}
